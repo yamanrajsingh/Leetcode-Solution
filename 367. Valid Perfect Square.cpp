145eae5da62c7ab2ff581ff07d46deb9dc13b8fa
Yaman Raj Singh <76215280+yamanrajsingh@users.noreply.github.com>
2023-06-28 02:22:49 +0530

    Add files via upload

---
+++ 367. Valid Perfect Square.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        
+        for (long long  i = 0; i * i >= 0 && i * i <= num; i ++) 
+        {
+            if(i*i==num)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
