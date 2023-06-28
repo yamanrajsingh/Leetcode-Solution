61daa84ce930443cb2fe4991561f2de15a65b285
Yaman Raj Singh <76215280+yamanrajsingh@users.noreply.github.com>
2023-06-29 00:39:42 +0530

    Add files via upload

---
+++ 704. Binary Search.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto it = find(nums.begin(),nums.end(),target);
+        if(it!=nums.end())
+        {
+            return it - nums.begin();
+        }
+        else
+        {
+            return -1;
+        }
+        
+    }
+};
