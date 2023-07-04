f648cc0081b1adf63d659db618afeb01572b9f67
Yaman Raj Singh <76215280+yamanrajsingh@users.noreply.github.com>
2023-07-04 20:39:20 +0530

    Add files via upload

---
+++ 137. Single Number II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        for(auto it:m)
+        {
+            if(it.second==1)
+            {
+                return it.first;
+            }
+        }
+        return 0;
+        
+    }
+};
