336d2d435aeb73ad630dab4fff374786cbc4c594
Yaman Raj Singh <76215280+yamanrajsingh@users.noreply.github.com>
2023-07-31 19:08:57 +0530

    Add files via upload

---
+++ 2221. Find Triangular Sum of an Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int n=nums.size();
+        while(n>1)
+        {
+        for(int i=1;i<n;i++)
+        {
+            nums[i-1] = (nums[i] +nums[i-1])%10;
+        }
+        n--;
+        }
+        return nums[0];
+        
+    }
+};
