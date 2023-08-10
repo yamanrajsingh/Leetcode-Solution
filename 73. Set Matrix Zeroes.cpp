8b68e16ae8e9cb9dfc22fd0ae668200f2301ea51
Yaman Raj Singh <76215280+yamanrajsingh@users.noreply.github.com>
2023-08-10 17:40:43 +0530

    Add files via upload

---
+++ 73. Set Matrix Zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int r = matrix.size();
+       int c =matrix[0].size();
+        vector<bool>row(r,false);
+         vector<bool>col(c,false);
+       for(int i=0;i<r;i++)
+       {
+           for(int j=0;j<c;j++)
+           {
+               if(matrix[i][j]==0)
+               {
+                  row[i]=true;
+                  col[j]=true;
+               }
+           }
+       }
+      for(int i=0;i<r;i++)
+       {
+           for(int j=0;j<c;j++)
+           {
+               if( row[i]|| col[j])
+                   matrix[i][j]=0;
+           }
+       }
+    }
+};
